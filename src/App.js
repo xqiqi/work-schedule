3f85e701310cbe76b685b2a4346d866b78a34438
Li <lwqiqi@foxmail.com>
2017-11-03 18:04:21 +0800

    Init the start planning page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,58 @@
 import React, { Component } from 'react';
+import { Icon, Layout, Menu } from 'antd';
+import Start from './Start';
 import './App.css';
 
+const { Sider } = Layout;
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuKey: 'start' // key navigation of the sider menu
+    };
+
+    this.handleMenuClick = this.handleMenuClick.bind(this);
+  }
+
+  handleMenuClick(e) {
+    this.setState({
+      menuKey: e.key
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>123</h1>
+        <Layout>
+          <Sider className="sider">
+            <div className="logo" />
+            <Menu
+              theme="dark"
+              mode="inline"
+              selectedKeys={[this.state.menuKey]}
+              onClick={this.handleMenuClick}
+            >
+              <Menu.Item key="start">
+                <Icon type="edit" />
+                <span className="nav-text">开始计划</span>
+              </Menu.Item>
+              <Menu.Item key="chart">
+                <Icon type="bar-chart" />
+                <span className="nav-text">统计图表</span>
+              </Menu.Item>
+              <Menu.Item key="history">
+                <Icon type="schedule" />
+                <span className="nav-text">历史记录</span>
+              </Menu.Item>
+            </Menu>
+          </Sider>
+
+          { this.state.menuKey === 'start' && <Start /> }
+
+          { this.state.menuKey === 'chart' && <Layout className="main"><p>统计图表</p></Layout> }
+          { this.state.menuKey === 'history' && <Layout className="main"><p>历史记录</p></Layout> }
+        </Layout>
       </div>
     );
   }
---
+++ src/Start.js
@@ -0,0 +1,112 @@
+import React, { Component } from 'react';
+import { Button, Col, Layout, Row, Select, Steps } from 'antd';
+
+const { Content, Header } = Layout;
+const Step = Steps.Step;
+
+class Start extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      step: 0, // current step when planning
+      shop: { id: '', name: '' },
+      startDate: ''
+    };
+
+    this.save = this.save.bind(this);
+    this.reset = this.reset.bind(this);
+    this.next = this.next.bind(this);
+    this.prev = this.prev.bind(this);
+    this.handleShopSelect = this.handleShopSelect.bind(this);
+  }
+
+  save() {}
+  reset() {}
+
+  next() {}
+  prev() {}
+
+  handleShopSelect() {}
+
+  render() {
+    const sContentShop = (
+      <div>
+        <Select
+          showSearch
+          style={{ width: 200 }}
+          placeholder='请选择店铺'
+          optionFilterProp='children'
+          onChange={this.handleShopSelect}
+          filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+        >
+        </Select>
+        <p><b>店铺基本情况</b> [更改]</p>
+      </div>
+    );
+    const sContentDate = <h1>2</h1>;
+    const sContentTime = <h1>3</h1>;
+    const sContentWork = <h1>4</h1>;
+
+    const steps = [{
+      title: '选择店铺',
+      content: sContentShop
+    }, {
+      title: '选择开始日期',
+      content: sContentDate
+    }, {
+      title: '确认工作时间',
+      content: sContentTime
+    }, {
+      title: '每日排班',
+      content: sContentWork
+    }];
+
+    return (
+      <Layout className="main">
+        <Header style={{ background: '#fff', textAlign: 'right', padding: '0 16px', borderBottom: '1px solid #eee' }}>
+          <Button type="primary" onClick={this.save}>保存</Button>
+          <Button type="default" onClick={this.reset} style={{ marginLeft: 16 }}>重置</Button>
+        </Header>
+
+        <Content style={{ margin: '24px 16px' }}>
+          <Row>
+            <Col span={18}>
+              <Steps current={this.state.step}>
+                {steps.map(item => <Step key={item.title} title={item.title} />)}
+              </Steps>
+              <div>{steps[this.state.step].content}</div>
+              <div className="steps-action">
+                {
+                  this.state.step < steps.length - 1
+                  &&
+                  <Button type="primary" onClick={this.next}>下一步</Button>
+                }
+                {
+                  this.state.step === steps.length - 1
+                  &&
+                  <Button type="primary">生成班表</Button>
+                }
+                {
+                  this.state.step > 0
+                  &&
+                  <Button style={{ marginLeft: 8 }} onClick={this.prev}>上一步</Button>
+                }
+              </div>
+            </Col>
+
+            <Col span={6} style={{ paddingLeft: 50 }}>
+              <div style={{ background: '#efefef', borderRadius: 4, padding: 16 }}>
+                <h3 align='center' style={{ marginBottom: 8 }}>排班情况概览</h3>
+                <p><b>已选店铺：</b>{this.state.shop.name}</p>
+                <p><b>开始日期：</b>{this.state.startDate}</p>
+                <p><b>人员周工作时间汇总：</b></p>
+              </div>
+            </Col>
+          </Row>
+        </Content>
+      </Layout>
+    )
+  }
+}
+
+export default Start;
