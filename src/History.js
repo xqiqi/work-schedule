610889ee18cafe13a20b3f93f69e8d6fd17c7b91
Li <lwqiqi@foxmail.com>
2017-11-20 16:57:23 +0800

    Update history filter

--- src/History.js
+++ src/History.js
@@ -1,11 +1,11 @@
 import low from 'lowdb';
 import LocalStorage from 'lowdb/adapters/LocalStorage';
 import React, { Component } from 'react';
-import Button from 'antd/lib/button';
 import DatePicker from 'antd/lib/date-picker';
 import Layout from 'antd/lib/layout';
 import Select from 'antd/lib/select';
 import Table from 'antd/lib/table';
+import TimeUtil from './utils/timeUtil';
 
 const adapter = new LocalStorage('schedule');
 const db = low(adapter);
@@ -19,21 +19,31 @@ class History extends Component {
     super(props);
 
     this.histories = [
-      { id: 1, shop: { id: '1', name: '上海愚园路店' }, startDate: '2017/11/20' },
-      { id: 2, shop: { id: '1', name: '上海愚园路店' }, startDate: '2017/11/13' },
-      { id: 3, shop: { id: '1', name: '上海愚园路店' }, startDate: '2017/11/06' },
-      { id: 4, shop: { id: '2', name: '上海淮海755店' }, startDate: '2017/11/20' },
-      { id: 5, shop: { id: '2', name: '上海淮海755店' }, startDate: '2017/11/13' },
-      { id: 6, shop: { id: '2', name: '上海淮海755店' }, startDate: '2017/11/06' },
-      { id: 7, shop: { id: '3', name: '上海世博源店' }, startDate: '2017/11/20' },
-      { id: 8, shop: { id: '3', name: '上海世博源店' }, startDate: '2017/11/13' },
-      { id: 9, shop: { id: '3', name: '上海世博源店' }, startDate: '2017/11/06' }
+      { id: 1, shop: { id: '1', name: '上海愚园路店' }, startDate: '2017/11/20', lastModified: '2017/11/19 22:00:00' },
+      { id: 2, shop: { id: '1', name: '上海愚园路店' }, startDate: '2017/11/13', lastModified: '2017/11/12 22:00:00' },
+      { id: 3, shop: { id: '1', name: '上海愚园路店' }, startDate: '2017/11/06', lastModified: '2017/11/13 22:00:00' },
+      { id: 4, shop: { id: '2', name: '上海淮海755店' }, startDate: '2017/11/20', lastModified: '2017/11/22 22:00:00' },
+      { id: 5, shop: { id: '2', name: '上海淮海755店' }, startDate: '2017/11/13', lastModified: '2017/11/11 22:00:00' },
+      { id: 6, shop: { id: '2', name: '上海淮海755店' }, startDate: '2017/11/06', lastModified: '2017/11/05 22:00:00' },
+      { id: 7, shop: { id: '3', name: '上海世博源店' }, startDate: '2017/11/20', lastModified: '2017/11/20 22:00:00' },
+      { id: 8, shop: { id: '3', name: '上海世博源店' }, startDate: '2017/11/13', lastModified: '2017/11/11 22:00:00' },
+      { id: 9, shop: { id: '3', name: '上海世博源店' }, startDate: '2017/11/06', lastModified: '2017/11/01 22:00:00' },
+      { id: 10, shop: { id: '1', name: '上海愚园路店' }, startDate: '2017/11/20', lastModified: '2017/11/09 22:00:00' },
+      { id: 11, shop: { id: '1', name: '上海愚园路店' }, startDate: '2017/11/13', lastModified: '2017/11/10 22:00:00' },
+      { id: 12, shop: { id: '1', name: '上海愚园路店' }, startDate: '2017/11/06', lastModified: '2017/11/03 22:00:00' },
+      { id: 13, shop: { id: '2', name: '上海淮海755店' }, startDate: '2017/11/20', lastModified: '2017/11/21 22:00:00' },
+      { id: 14, shop: { id: '2', name: '上海淮海755店' }, startDate: '2017/11/13', lastModified: '2017/11/15 22:00:00' },
+      { id: 15, shop: { id: '2', name: '上海淮海755店' }, startDate: '2017/11/06', lastModified: '2017/11/08 22:00:00' },
+      { id: 16, shop: { id: '3', name: '上海世博源店' }, startDate: '2017/11/20', lastModified: '2017/11/12 22:00:00' },
+      { id: 17, shop: { id: '3', name: '上海世博源店' }, startDate: '2017/11/13', lastModified: '2017/11/12 22:00:00' },
+      { id: 18, shop: { id: '3', name: '上海世博源店' }, startDate: '2017/11/06', lastModified: '2017/11/06 22:00:00' }
     ];
+    this.histories.sort((a, b) => new Date(b.lastModified) - new Date(a.lastModified));
 
     this.state = {
       filter: {
         shop: '',
-        startDate: ''
+        startDateRange: []
       },
       histories: Array.from(this.histories)
     };
@@ -60,18 +70,41 @@ class History extends Component {
     this.setState({ filter });
     this.setState({ histories });
   }
-  
-  handleRangePick() {}
+
+  /**
+   * when the date range picker changes
+   * @param range
+   */
+  handleRangePick(range) {
+    const filter = this.state.filter;
+    let histories = this.histories;
+
+    if (range.length !== 0) {
+      histories = histories.filter(history =>
+        (TimeUtil.parseDateStrToMoment(history.startDate) > range[0])
+        &&
+        (TimeUtil.parseDateStrToMoment(history.startDate) < range[1])
+      );
+    }
+
+    filter.startDateRange = range;
+    this.setState({ filter });
+    this.setState({ histories });
+  }
 
   render() {
     const columns = [{
       title: '店铺',
       dataIndex: 'shop.name',
-      key: 'shop.id'
+      width: '20%'
     }, {
-      title: '首日日期',
+      title: '班表首日日期',
       dataIndex: 'startDate',
-      key: 'startDate'
+      width: '20%'
+    }, {
+      title: '最新修改时间',
+      dataIndex: 'lastModified',
+      width: '30%'
     }, {
       title: '操作',
       key: 'action',
