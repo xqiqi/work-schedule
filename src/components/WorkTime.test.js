9dadf7feaddf82f95645d3e12b2666afc1c476ab
Li <lwqiqi@foxmail.com>
2018-01-01 19:31:32 +0800

    Update test cases

--- src/components/WorkTime.test.js
+++ src/components/WorkTime.test.js
@@ -5,7 +5,7 @@ import WorkTime from './WorkTime';
 import React from 'react';
 import TimePicker from 'antd/lib/time-picker';
 import TimeUtil from '../utils/timeUtil';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 
 describe('<WorkTime />', () => {
   const props = { day: '1', start: '09:00', end: '22:00' };
@@ -18,28 +18,23 @@ describe('<WorkTime />', () => {
   it('should set the right initial state values', () => {
     const wrapper = shallow(<WorkTime {...props} />);
     const { day, ...expectState } = props;
-    expect(wrapper.instance().state).toEqual(expectState);
+    expect(wrapper.state()).toEqual(expectState);
   });
 
   it('should set right start and end time to TimePicker', () => {
-    const wrapper = mount(<WorkTime {...props} />);
+    const wrapper = shallow(<WorkTime {...props} />);
 
-    const startTimePicker = wrapper.find(TimePicker).at(0).instance();
-    expect(startTimePicker.props.defaultValue).toEqual(TimeUtil.parseTimeStrToMoment(props.start));
-
-    const endTimePicker = wrapper.find(TimePicker).at(1).instance();
-    expect(endTimePicker.props.defaultValue).toEqual(TimeUtil.parseTimeStrToMoment(props.end));
+    expect(wrapper.find(TimePicker).at(0).props().defaultValue).toEqual(TimeUtil.parseTimeStrToMoment(props.start));
+    expect(wrapper.find(TimePicker).at(1).props().defaultValue).toEqual(TimeUtil.parseTimeStrToMoment(props.end));
   });
 
   it('should set new state values after TimePicker changes', () => {
-    const wrapper = mount(<WorkTime {...props} />);
+    const wrapper = shallow(<WorkTime {...props} />);
 
-    const startTimePicker = wrapper.find(TimePicker).at(0).instance();
-    startTimePicker.props.onChange(null, '12:00');
-    expect(wrapper.instance().state.start).toEqual('12:00');
+    wrapper.find(TimePicker).at(0).props().onChange(null, '12:00');
+    expect(wrapper.state('start')).toEqual('12:00');
 
-    const endTimePicker = wrapper.find(TimePicker).at(1).instance();
-    endTimePicker.props.onChange(null, '12:00');
-    expect(wrapper.instance().state.end).toEqual('12:00');
+    wrapper.find(TimePicker).at(1).props().onChange(null, '12:00');
+    expect(wrapper.state('end')).toEqual('12:00');
   });
 });
